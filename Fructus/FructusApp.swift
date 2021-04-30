eca11db7f8f70a05c2291d1690f0849aaac02f7f
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2021-04-29 21:30:33 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Erik Salas on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
